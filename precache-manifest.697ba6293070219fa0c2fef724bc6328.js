ae469528fd5b344487e2a572880936fbc12735f1
Jan Hrdina <jan.hrdka@gmail.com>
2020-08-01 01:35:35 +0200

    Updates

---
+++ precache-manifest.697ba6293070219fa0c2fef724bc6328.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ce10c15652feae157f42c93f314e95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a4f6fda8c6f8aac158d",
+    "url": "/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "62b428ff2e789c2c1b1f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a4f6fda8c6f8aac158d",
+    "url": "/static/js/2.e459b11e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e459b11e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b428ff2e789c2c1b1f",
+    "url": "/static/js/main.3858f56f.chunk.js"
+  },
+  {
+    "revision": "475e629f97b572cbd433",
+    "url": "/static/js/runtime-main.82a2a924.js"
+  }
+]);
